a12081eb5f245ba1118270d886fb1ca9c671a368
MacesAndrei <liderulch2@gmail.com>
2021-06-19 19:40:02 +0300

    .

--- Tools/design.js
+++ Tools/design.js
@@ -12,17 +12,33 @@ var inverted = 0;
 function Invert()
 {
 	if(inverted == 0){
-		$(":root").css("--color1", "#333");
-		$(":root").css("--color2", "#eee");
-		$(":root").css("--colorbg", "#222");
-		$(".invert").css("filter" , "invert(100%)");
-		inverted = 1;
-	}
-	else {
 		$(":root").css("--color1", "#eee");
 		$(":root").css("--color2", "#333");
 		$(":root").css("--colorbg", "#fff");
 		$(".invert").css("filter" , "invert(0)");
+		inverted = 1;
+	}
+	else {
+		$(":root").css("--color1", "#333");
+		$(":root").css("--color2", "#eee");
+		$(":root").css("--colorbg", "#222");
+		$(".invert").css("filter" , "invert(100%)");
 		inverted = 0;
+	}
+}
+
+Invert()
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function ModulLecturare()
+{
+	for(var i = 0 ; i < (document.getElementsByClassName('panel-collapse')).length ; i++)
+	{
+		document.getElementsByClassName('panel-collapse')[i].classList.add('in');
+		console.log(1);
+		await sleep(1000);
 	}
 }
